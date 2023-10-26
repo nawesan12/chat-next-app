7bfb331ac70bdcc38576a600f6c51dfed64353b1
Nahuel Santillan <nahueltecnicomdp@gmail.com>
2023-10-26 15:57:09 -0300

    chat con imagenes

---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+export default function Layout({ children }) {
+  return (
+    <>
+      <nav>
+        MENU
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+
+      {children}
+
+      <footer>Hola soy nawe</footer>
+    </>
+  );
+}
